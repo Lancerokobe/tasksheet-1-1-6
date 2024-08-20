4b71cace5fcdadd2e514f3f9f98e9d1bbbc76b49
Lancerokobe <141099012+Lancerokobe@users.noreply.github.com>
2024-08-20 13:07:01 +0000

    Input: Wow output: The input is Palindrome.

--- Tasksheet116.java
+++ Tasksheet116.java
@@ -1,1 +1,13 @@
+public class Tasksheet116 {
+    public static void main(String[] args) {
+        String input = "wow";
+        StringBuilder reversed = new StringBuilder(input);
+        reversed.reverse();
 
+        if (input.equals(reversed.toString())){
+            System.out.println("The input is Palindrome.");
+        } else {
+            System.out.println("The input is not Palindrome.");
+        }
+    }
+}
